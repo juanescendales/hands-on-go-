dc31271b652ba9935374560ae9510e792c1608e5
Juan Esteban Cendales <jcendaless@unal.edu.co>
2023-07-17 05:31:07 +0000

    feat: functions

--- challenges/types-primitive/begin/main.go
+++ challenges/types-primitive/begin/main.go
@@ -1,18 +1,34 @@
 // challenges/types-primitive/begin/main.go
 package main
 
-// use type inference to create a package-level variable "val" and assign it a value of "global"
+import "fmt"
 
+// use type inference to create a package-level variable "val" and assign it a value of "global"
+var val = "global"
 func main() {
 	// create a local variable "val" and assign it an int value
-
+	var val int
 	// print the value of the local variable "val"
-
+	fmt.Println(val)
 	// print the value of the package-level variable "val"
-
+	printGlobalVal()
 	// update the package-level variable "val" and print it again
-
+	updateGlobalVal("GloVal")
+	printGlobalVal()
 	// print the pointer address of the local variable "val"
-
+	var pointer *int
+	pointer = &val
+	fmt.Println(pointer)
 	// assign a value directly to the pointer address of the local variable "val" and print the value of the local variable "val"
+	*pointer = 123
+	fmt.Println(val)
+
+}
+
+func printGlobalVal(){
+	fmt.Println(val)
+}
+
+func updateGlobalVal(newValue string){
+	val = newValue
 }
--- functions/begin/main.go
+++ functions/begin/main.go
@@ -1,14 +1,27 @@
 // functions/begin/main.go
 package main
 
+import (
+	"fmt"
+	"strconv"
+)
+
 // simple greet function
-//
+func greet() string {
+	return "Hello World"
+}
 
 // greetWithName returns a greeting with the name
-//
+func greetWithName(name string) string {
+	var greeting string = fmt.Sprint("Hello %s!", name)
+	return greeting
+}
 
 // greetWithName returns a greeting with the name and age of the person
-//
+func greetWithNameAndAge(name string, age int) (greeting string) {
+	greeting = fmt.Sprint("Hello %s !, your age is %s", name, strconv.Itoa(age))
+	return
+}
 
 // divide divides two numbers and returns the result
 // if the second number is zero, it returns  error
@@ -16,10 +29,10 @@ package main
 
 func main() {
 	// invoke greet function
-	// fmt.Println(greet())
+	fmt.Println(greet())
 
 	// invoke greetWithName function
-	// fmt.Println(greetWithName("Toni"))
+	fmt.Println(greetWithName("Toni"))
 
 	// invoke divide function
 	// fmt.Println(divide(10, 2))
